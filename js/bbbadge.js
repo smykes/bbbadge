40b97639c98b06fdecae7335c442267d22b7f6c1
smykes <jeff.smykil@gmail.com>
2013-01-27 19:33:30 -0500

    Removed unused CSS, added link to stats page from name.

--- js/bbbadge.js
+++ js/bbbadge.js
@@ -18,11 +18,12 @@ $('document').ready(function() {
 		crossDomain: true
 	}).done(
 		function (msg) {
-			$("#bbbadge").addClass("horizontal-large");
+			$("#bbbadge").addClass( options.orientation + "-" + options.size);
+			var stats_url = "http://dribbble.com/" + player
 			var image_url = msg.avatar_url;
 			var img_src = "<img class='avatar' height='64px' width='64px' src='" + image_url + "'/>"
 			$("#bbbadge").prepend(img_src);
-			$("#bbbadge").append('<div id="name">' + msg.name + '</div>');
+			$("#bbbadge").append('<div id="name"><a href="' + stats_url +'">' + msg.name + '</a></div>');
 			$("#bbbadge").append('<div id="location">' + msg.location + '</div>');
 			$("#bbbadge").append('<div id="countContainer"></div>');
 			$("#countContainer").append('<div id="shots">' + msg.shots_count + ' <div>Shots</div></div>');
